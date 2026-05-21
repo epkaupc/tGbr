4d6d99e03e77ac36d7fd730dd9787665f8d388b1
2e! <wpkauepc@gmail.com>
2026-05-21 09:40:58 -0300

    erro na funçao

--- android/app/src/main/java/com/anonymous/app/FloatingBubbleService.java
+++ android/app/src/main/java/com/anonymous/app/FloatingBubbleService.java
@@ -159,7 +159,7 @@ public class FloatingBubbleService extends Service {
         title.setGravity(Gravity.START);
 
         TextView subtitle = new TextView(this);
-        subtitle.setText(notificationGrabberPackage());
+        subtitle.setText(notificationGrabberPackage);
         subtitle.setTextColor(Color.LTGRAY);
         subtitle.setTextSize(14);
 
